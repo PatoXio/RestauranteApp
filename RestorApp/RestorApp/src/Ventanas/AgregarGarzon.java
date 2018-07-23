96a948b793132453e8fdf0360e4cc32066708b2c
Crishako <39605561+Crishako@users.noreply.github.com>
2018-07-23 02:12:02 -0400

    loool

--- RestorApp/RestorApp/src/Ventanas/AgregarGarzon.java
+++ RestorApp/RestorApp/src/Ventanas/AgregarGarzon.java
@@ -172,6 +172,7 @@ public class AgregarGarzon extends JFrame
 					textoNombre.setText("");
 					textoSueldo.setText("");
 					textoEdad.setText("");
+					textoNivelIngles.setText("");
 				}	
 			}
 		});
